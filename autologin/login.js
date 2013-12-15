73fbb9b035d7000a9aadbdc7554eb6353840594b
Paul Liu <Paul@deep-thought.(none)>
2013-12-14 20:03:32 -0800

    added ubc's shiny new login domain

--- autologin/login.js
+++ autologin/login.js
@@ -1,9 +1,24 @@
 if (~(window.location.href).indexOf("login")) {
 	chrome.extension.sendRequest({method: "getInfo"}, function(response) {
-  document.forms[1].loginName.value = response.username;
-  document.forms[1].password.value = response.password;
-  if (response.username != undefined)
-		document.forms[1].action.click();
+	
+  login_box = document.forms[1];
+  //ugly hack to get around the fact that ubc does not have
+  //consistent naming for each of their login pages.
+  if ('loginName' in login_box) {
+	login_box.loginName.value = response.username;
+  } else {
+	login_box.username.value = response.username;
+  }
+  
+  login_box.password.value = response.password;
+  if (response.username != undefined) {
+	//just try both ways of clicking the button
+	try { //wow such hacks 
+		login_box.submit.click();
+	} catch(err) {
+		login_box.action.click();
+	}
+  }
 });
 } else if (~(window.location.href).indexOf("UserPassword")) {
 	chrome.extension.sendRequest({method: "getInfo"}, function(response) {
